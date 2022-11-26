2f95ead9e754119b8b71cd541a95dc212ea05cf4
NayeemHossenn <nayeemhossen245@gmail.com.com>
2022-11-26 21:19:53 +0600

    load category from server

--- src/Pages/Home/Categories/Categories.js
+++ src/Pages/Home/Categories/Categories.js
@@ -1,10 +1,9 @@
 import React, { useEffect, useState } from "react";
-import CategoriesItems from "./CategoriesItems";
 
 const Categories = () => {
   const [products, setProducts] = useState([]);
   useEffect(() => {
-    fetch("categories.json")
+    fetch("http://localhost:5000/categories")
       .then((res) => res.json())
       .then((data) => setProducts(data));
   }, []);
@@ -14,7 +13,7 @@ const Categories = () => {
         <div className="text-center mt-8">
           <p className="text-2xl font-bold ">Resale Phone Categories</p>
         </div>
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 p-4">
           {products.map((product) => (
             <div key={product._id}>
               <div className="card w-96 bg-base-100 shadow-xl">
@@ -25,7 +24,7 @@ const Categories = () => {
                   <h2 className="card-title ">{product.title}</h2>
 
                   <div className="card-actions justify-end">
-                    <button className="btn btn-primary">Buy Now</button>
+                    <button className="btn btn-primary">See All</button>
                   </div>
                 </div>
               </div>
